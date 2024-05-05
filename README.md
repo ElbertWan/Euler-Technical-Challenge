4d8c01239316297905a58fa13e45e1b1e4e272a7
Elbert Wan <elbewa@gmail.com>
2024-05-05 16:52:49 +1000

    added tests, refactored code to be cleaner, changes output to be cleaner

--- README.md
+++ README.md
@@ -1,13 +1,16 @@
 # Euler-Technical-Challenge
+
 Technical Challenge for Euler (Maximum Path Sum II)
 
 ---
 
 ## Running the project
+
 - run `npm i` in the source directory
 - run `npm run start` calculate the maximum sum of the number triangle stored in `triangle.txt`
 
 ---
 
 ## Changable files
+
 - You can alter `triangle.txt` to any triangular number scheme. Note that each number must have a whitespace in between.
--- main.ts
+++ main.ts
@@ -1,36 +1,8 @@
-import { readFileSync } from 'fs';
-
-//convert txt file into double array numbers;
-const readFile = (traversalArray:  number[][]) => {
-    const file = readFileSync('triangle.txt', 'utf-8');
-    const lineArray: string[] = [];
-    for (const line of file.split(/[\r\n]+/)){
-        lineArray.push(line);
-        const tempArray: number[] = [];
-        for (const number of line.split(" ")){
-            tempArray.push(parseInt(number));
-        }
-        traversalArray.push(tempArray);
-    }
-}
-
-//find largest sum from bottom of triangle using traversal array
-const findLargestSum = (traversalArray:  number[][]) => {
-    for (let row = traversalArray.length - 1 ; row > 0 ; row--){
-        for (let column = 0 ; column < traversalArray[row].length-1 ; column++){
-            const maxNumber = Math.max(traversalArray[row][column], traversalArray[row][column+1]);
-            const newSum = traversalArray[row-1][column] + maxNumber;
-            traversalArray[row-1][column] = newSum;
-        }
-    }
-}
+import readFile from "./ReadFile";
+import findLargestSum from "./findLargestSum";
 
 const traversalArray: number[][] = [];
-// const nodearray: TreeNode[] = [];
-readFile(traversalArray);
+readFile(traversalArray, "triangle.txt");
 findLargestSum(traversalArray);
 
-
-console.log(traversalArray[0][0])
-
-
+console.log('Largest sum in the triangle is:', traversalArray[0][0]);
